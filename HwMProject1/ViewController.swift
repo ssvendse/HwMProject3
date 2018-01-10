fd08c50b507d92431e5734eff555f62bc60d9157
Skyler Svendsen <skylersvendsen@gmail.com>
2018-01-09 20:42:45 -0600

    Initial Commit

---
+++ HwMProject1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HwMProject1
+//
+//  Created by Skyler Svendsen on 1/9/18.
+//  Copyright © 2018 Skyler Svendsen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
